026ceefde4e2b8693d07cf141c8e363bc0a91b23
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-08-06 17:23:09 -0500

    join table data, edit app.get

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 // Get all Candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if (err) {
@@ -36,8 +40,12 @@ app.get('/api/candidates', (req, res) => {
 
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id 
+                WHERE candidates.id = ?`;
     // assign array w/ single element           
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
