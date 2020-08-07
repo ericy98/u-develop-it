029e05dfe5777e928f063042f3f2b82ca3766f57
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-08-07 11:25:14 -0500

    create POST, PUT, and DELETE req for voters api

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -14,12 +14,13 @@ router.get('/voters', (req, res) => {
         }
 
         res.json({
-            message: 'success', 
+            message: 'success',
             data: rows
         });
     });
 });
 
+// GET a single voter
 router.get('/voter/:id', (req, res) => {
     const sql = `SELECT * FROM voters WHERE id = ?`;
     const params = [req.params.id];
@@ -29,12 +30,80 @@ router.get('/voter/:id', (req, res) => {
             res.status(400).json({ error: err.message });
             return;
         }
-        
+
         res.json({
-            message: 'success', 
+            message: 'success',
             data: row
         });
     });
 });
+
+// register ppl thru the app
+router.post('/voter', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.run(sql, params, function (err, data) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
+
+// update email address
+router.put('/voter/:id', (req, res) => {
+    // Data validation
+    const errors = inputCheck(req.body, 'email');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    // Prepare statement
+    const sql = `UPDATE voters SET email = ? WHERE id = ?`;
+    const params = [req.body.email, req.params.id];
+
+    // Execute
+    db.run(sql, params, function (err, data) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: req.body,
+            changes: this.changes
+        });
+    });
+});
+
+
+// deactivate account 
+router.delete('/voter/:id', (req, res) => {
+    const sql = `DELETE FROM voters WHERE id = ?`;
+  
+    db.run(sql, req.params.id, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({ message: 'deleted', changes: this.changes });
+    });
+  });
 
 module.exports = router;
